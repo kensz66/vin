71d19d5484e84aa10b739d9bb500dd809609b4c5
Ken <kensz66@126.com>
2021-07-24 22:08:03 +0800

    @

--- include/extend.func.php
+++ include/extend.func.php
@@ -1,21 +1,21 @@
 <?php
-function litimgurls($imgid=0)
+function litimgurls($imgid = 0)
 {
-    global $lit_imglist,$dsql;
+    global $lit_imglist, $dsql;
     //获取附加表
     $row = $dsql->GetOne("SELECT c.addtable FROM #@__archives AS a LEFT JOIN #@__channeltype AS c 
                                                             ON a.channel=c.id where a.id='$imgid'");
     $addtable = trim($row['addtable']);
-    
+
     //获取图片附加表imgurls字段内容进行处理
     $row = $dsql->GetOne("Select imgurls From `$addtable` where aid='$imgid'");
-    
+
     //调用inc_channel_unit.php中ChannelUnit类
-    $ChannelUnit = new ChannelUnit(2,$imgid);
-    
+    $ChannelUnit = new ChannelUnit(2, $imgid);
+
     //调用ChannelUnit类中GetlitImgLinks方法处理缩略图
     $lit_imglist = $ChannelUnit->GetlitImgLinks($row['imgurls']);
-    
+
     //返回结果
     return $lit_imglist;
 }
@@ -25,121 +25,112 @@ function litimgurls($imgid=0)
  *  文章内容提取图片(多张)自定义输出
  *
  * @access    public
- * @param     string  $string  	文档内容
- * @param     string  $num  	输出几张
+ * @param     string  $htmls  	文档内容
+ * @param     string  $num  	输出几张   $num <= 0,输出全部图片
  * @return    string
  */
 
-
-//获取原始数据
-if (!function_exists('get_html_pics')) {
-    function get_html_pics($string, $num)
+class KenHelpers
+{
+    private static $picsUrl;
+    private static $picsHtml;
+    private static $htmls;      //记录外部传入的参数；
+    public static $i = 0;
+    protected function __construct()
     {
-        preg_match_all("/src=('|\")([^'\"]+)('|\")/", $string, $matches);
+    }
+    protected function __clone()
+    {
+    }
+
+
+    public static function get_picsUrl($htmls)
+    {
+        preg_match_all("/src=('|\")([^'\"]+)('|\")/", $htmls, $matches);
         $imgsrc_arr = array_unique($matches[0]);
-        return $imgsrc_arr;
+        // echo ++self::$i.'<br>';
+        self::$picsUrl = $imgsrc_arr;
+        return self::$picsUrl;
     }
-}
 
-// 获取小图：根据要求对数据进行处理：如果$num<=0，则输出所有图片，如果$num>0，则输出$num张图片
-if (!function_exists('get_html_lit_pics')) {
-    function get_html_lit_pics($string, $num)
+    public static function get_picsHtml($htmls, $num = 0)
     {
-        $imgsrc_arr = get_html_pics($string, $num);
-        $result = '';
-        $start = '<div class="swiper-slide gallery-thumbs-item"><a href="#"><img ';
-        $end = '></a></div>';
-        // $count = count($imgsrc_arr);
-        if ($num <= 0) {
-            foreach ($imgsrc_arr as $i => $imgsrc) {
-                $result .= $start . $imgsrc . $end . '
-            ';
-            }
-            return $result;
-        } else {
-            foreach ($imgsrc_arr as $i => $imgsrc) {
-                if ($i == $num) break;
-                $result .= $start . $imgsrc . $end . '
-            ';
-            }
-            return $result;
-        }
-    }
-}
 
-// 获取大图：根据要求对数据进行处理：如果$num<=0，则输出所有图片，如果$num>0，则输出$num张图片
-if (!function_exists('get_html_big_pics')) {
-    function get_html_big_pics($string, $num)
-    {
-        $imgsrc_arr = get_html_pics($string, $num);
-        $result = '';
-        $start_1 = '<div class="swiper-slide gallery-item"><a href="#" data-bs-toggle="modal"
-                data-bs-target="#big-pic-modal';
-        $start_2 = '"><img ';
-        $end_1 = 'alt="';
-        $end_2 = '"></a></div>';
-        $end_3 = <<<EOT
-EOT;
-        // $count = count($imgsrc_arr);
-        if ($num <= 0) {
-            foreach ($imgsrc_arr as $i => $imgsrc) {
-                $result .= $start_1 . $i . $start_2 . $imgsrc . $end_1 . $i . $end_2;
-            }
-            return $result;
-        } else {
-            foreach ($imgsrc_arr as $i => $imgsrc) {
-                if ($i == $num) break;
-                $result .= $start_1 . $i . $start_2 . $imgsrc . $end_1 . $i . $end_2;
-            }
-            return $result;
-        }
-    }
-}
+        // 如果传入的代码为空，则直接返回空值
+        if (empty($htmls)) return null;
 
-//获取大图片 modal 内容弹出框
-if (!function_exists('get_html_modal_pics')) {
-    function get_html_modal_pics($string, $num)
-    {
-        $imgsrc_arr = get_html_pics($string, $num);
-        $result = '';
-        $start_1 = '<div class="modal fade" id="big-pic-modal';
-        $start_2 = '">
-        <div class="modal-dialog modal-dialog-centered product-modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
-            </div>
-            <div class="modal-body text-center">
-            <img class="align-middle"  ';
-        $end_1 = 'alt="';
-        $end_2 = '">
-             </div>
-             <div class="modal-footer">
-               <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#big-pic-modal';
-        $end_3 = <<<EOT
-" data-bs-dismiss="modal">下一张</button>
-             </div>
-          </div>
+        // 如果传入的代码与前面相同，直接返回已经计算的结果
+        if ($htmls === self::$htmls) return self::$picsHtml;
+
+        // 更新变量存储，用于下次判断
+        self::$htmls = $htmls;
+
+        $imgsrc_arr = self::get_picsUrl($htmls);
+        //小图html
+        $x = '<div class="swiper-slide gallery-thumbs-item">
+        <a href="javascript:void(0)"><img ';
+        $y = ' alt="缩略图';
+        $z = '"></a>
+        </div>';
+        //大图html
+        $a = '<div class="swiper-slide gallery-item">
+        <a href="javascript:void(0)" data-bs-toggle="modal"  data-bs-target="#big-pic-modal';
+        $b = '"><img ';
+        $c = 'alt="图';
+        $d = '"></a>
         </div>
-      </div>
-EOT;
-        $count = count($imgsrc_arr);
-        if ($num <= 0) {
-            foreach ($imgsrc_arr as $i => $imgsrc) {
-                if ($i < $count - 1) {
-                    $j = $i + 1;
-                    $result .= $start_1 . $i . $start_2 .  $imgsrc . $end_1 . $i . $end_2 . $j . $end_3;
-                } else {
-                    $result .= $start_1 . $i . $start_2 .  $imgsrc . $end_1 . $i . $end_2 . '0' . $end_3;
-                }
-            }
-            return $result;
-        } else {
-            foreach ($imgsrc_arr as $i => $imgsrc) {
-                if ($i == $num) break;
-                $result .= $start_1 . $i . $start_2 .  $imgsrc . $end_1 . $i . $end_2;
-            }
-            return $result;
+        ';
+
+        //madal html
+        $s1 = '<div class="modal fade" id="big-pic-modal';
+        $s2 = '">
+        <div class="modal-dialog modal-dialog-centered product-modal-dialog">
+        <div class="modal-content">
+        <div class="modal-header">
+        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+        </div>
+        <div class="modal-body text-center">
+        <img class="align-middle" ';
+        $s3 = 'alt="图';
+        $s4 = '">
+        </div>
+        <div class="modal-footer">
+        <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#big-pic-modal';
+        $s5 = '" data-bs-dismiss="modal">下一张</button>
+        </div>
+        </div>
+        </div>
+        </div>';
+
+        $result = []; // 用数组存储组合结果
+
+        $count = count($imgsrc_arr) - 1;
+        foreach ($imgsrc_arr as $i => $imgsrc) {
+
+            if ($num > 0 && $i == $num) break;
+            //小图
+            $result[0] .= $x . $imgsrc . $y . $i . $z;
+            //大图
+            $result[1] .= $a . $i . $b . $imgsrc . $c . $i . $d;
+            //modal
+            $i < $count ?
+                $result[2] .= $s1 . $i . $s2 .  $imgsrc . $s3 . $i . $s4 . ($i + 1) . $s5 :
+                $result[2] .= $s1 . $i . $s2 .  $imgsrc . $s3 . $i . $s4 . '0' . $s5;
         }
+        self::$picsHtml = $result;
+
+        // var_dump($result);
+        return self::$picsHtml;
     }
+}
+
+
+function get_pics_code($htmls, $num, $size)
+{
+    $pics_code = KenHelpers::get_picsHtml($htmls, $num);
+    return $size === 'lit' ? $pics_code[0] : $pics_code[1];
+}
+function get_modal_pics_code($htmls, $num)
+{
+    return  KenHelpers::get_picsHtml($htmls, $num)[2];
 }
